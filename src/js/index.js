98cde27b95a1889e53cc6965f80d77b8a92f4869
Glauco Morera <gmorerafortes@gmail.com>
2023-05-13 22:07:49 -0300

    Projeto lista pokemon

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+const btnAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemTrocaTema = document.querySelector(".imagem-botao");
+
+btnAlterarTema.addEventListener("click", () => {
+    const modoEscuroAtivo = body.classList.contains("modo-escuro");
+    if (modoEscuroAtivo) {
+        body.classList.remove("modo-escuro");
+        imagemTrocaTema.setAttribute("src", "./src/image/sun.png");
+    } else {
+        body.classList.add("modo-escuro");
+        imagemTrocaTema.setAttribute("src", "./src/image/moon.png");
+    }
+});
